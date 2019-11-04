d4a666db38b0a2fe73b62ccbb993c893081ea17b
Andrew Brown <BrownA2@bathnes.gov.uk>
2019-11-04 16:28:01 +0000

    mostly right impact routing

--- app/routes.js
+++ app/routes.js
@@ -58,11 +58,20 @@ router.post('/concerning-impacts', function (req, res) {
   console.log(path);
   if (path[path.length - 1] == 'issues_of_concern') {
     req.session.data['concerns-impact-questions-key'] = 0;
+    req.session.data['concerns-impacts-current'] = 1;
   } else {
     req.session.data['concerns-impact-questions-key'] = answers.indexOf(path[path.length - 1]) + 1;
-    level = req.session.data[path[path.length - 1]];
-    console.log(path[path.length - 1] + ' = ' + level);
+    req.session.data['concerns-impacts-current'] = req.session.data['concerns-impact-questions-key'] + 1;
   }
+  if (answers.length > 1) {
+    req.session.data['concerns-impacts-current-legend'] = "  (" + req.session.data['concerns-impacts-current'] + " of " + answers.length + " concerns you identified)";
+  } else {
+    req.session.data['concerns-impacts-current-legend'] = "";
+  }
+  console.log(req.session.data['concerns-impacts-current-legend']);
+  level = req.session.data[path[path.length - 1]];
+  console.log(path[path.length - 1] + ' = ' + level);
+
   console.log(answers);
   console.log(req.session.data['concerns-impact-questions-key']);
   if (req.session.data['concerns-impact-questions-key'] < 0 || req.session.data['concerns-impact-questions-key'] == answers.length) {
